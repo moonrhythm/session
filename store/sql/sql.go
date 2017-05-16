d620f9e84f7da9770c3c0ef5c77b340852dafd77
acoshift <acoshift@gmail.com>
2017-05-17 03:07:19 +0700

    sql: add cleanup worker

--- store/sql/sql.go
+++ store/sql/sql.go
@@ -2,6 +2,7 @@ package sql
 
 import (
 	"database/sql"
+	"errors"
 	"fmt"
 	"log"
 	"time"
@@ -13,7 +14,7 @@ import (
 func New(db *sql.DB, table string) session.Store {
 	_, err := db.Exec(fmt.Sprintf(`
 		CREATE TABLE IF NOT EXISTS %s (
-			k TEXT PRIMARY KEY NOT NULL,
+			k TEXT PRIMARY KEY UNIQUE NOT NULL,
 			v BLOB,
 			e TIMESTAMP,
 			INDEX (e)
@@ -22,7 +23,7 @@ func New(db *sql.DB, table string) session.Store {
 	if err != nil {
 		log.Printf("session: can not create sql table; %v\n", err)
 	}
-	getStmt, _ := db.Prepare(fmt.Sprintf(`SELECT v FROM %s WHERE k = $1;`, table))
+	getStmt, _ := db.Prepare(fmt.Sprintf(`SELECT v, e, now() FROM %s WHERE k = $1;`, table))
 	setStmt, _ := db.Prepare(fmt.Sprintf(`
 		INSERT INTO %s (k, v, e)
 		VALUES ($1, $2, $3)
@@ -31,22 +32,42 @@ func New(db *sql.DB, table string) session.Store {
 	`, table))
 	delStmt, _ := db.Prepare(fmt.Sprintf(`DELETE FROM %s WHERE k = $1;`, table))
 	expStmt, _ := db.Prepare(fmt.Sprintf(`UPDATE %s SET e = $2 WHERE k = $1;`, table))
-	return &sqlStore{db, getStmt, setStmt, delStmt, expStmt}
+	delExpiredStmt, _ := db.Prepare(fmt.Sprintf(`DELETE FROM %s WHERE e <= now();`, table))
+	s := &sqlStore{db, getStmt, setStmt, delStmt, expStmt, delExpiredStmt}
+	go s.cleanupWorker()
+	return s
 }
 
 type sqlStore struct {
-	db      *sql.DB
-	getStmt *sql.Stmt
-	setStmt *sql.Stmt
-	delStmt *sql.Stmt
-	expStmt *sql.Stmt
+	db             *sql.DB
+	getStmt        *sql.Stmt
+	setStmt        *sql.Stmt
+	delStmt        *sql.Stmt
+	expStmt        *sql.Stmt
+	delExpiredStmt *sql.Stmt
+}
+
+var errNotFound = errors.New("sql: session not found")
+
+func (s *sqlStore) cleanupWorker() {
+	time.Sleep(5 * time.Second)
+	for {
+		s.delExpiredStmt.Exec()
+		time.Sleep(6 * time.Hour)
+	}
 }
 
 func (s *sqlStore) Get(key string) ([]byte, error) {
 	var bs []byte
-	err := s.getStmt.QueryRow(key).Scan(&bs)
+	var exp *time.Time
+	var now time.Time
+	err := s.getStmt.QueryRow(key).Scan(&bs, &exp, &now)
 	if err != nil {
 		return nil, err
+	}
+	if exp != nil && exp.After(now) {
+		s.Del(key)
+		return nil, errNotFound
 	}
 	return bs, nil
 }
