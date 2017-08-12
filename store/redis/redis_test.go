2ba40c59ef27cfecc94a405739f0cd8db2e8809d
acoshift <acoshift@gmail.com>
2017-08-13 02:27:29 +0700

    add redis test

--- store/redis/redis_test.go
+++ store/redis/redis_test.go
@@ -1,20 +1,25 @@
-package memory_test
+package redis_test
 
 import (
 	"testing"
 	"time"
 
-	store "github.com/acoshift/session/store/memory"
+	store "github.com/acoshift/session/store/redis"
+	"github.com/garyburd/redigo/redis"
 )
 
-func TestMemory(t *testing.T) {
-	s := store.New(store.Config{CleanupInterval: 10 * time.Millisecond})
-	err := s.Set("a", []byte("test"), time.Millisecond)
+func TestRedis(t *testing.T) {
+	s := store.New(store.Config{Prefix: "session:", Pool: &redis.Pool{
+		Dial: func() (redis.Conn, error) {
+			return redis.Dial("tcp", "localhost:6379")
+		},
+	}})
+	err := s.Set("a", []byte("test"), time.Second)
 	if err != nil {
 		t.Fatalf("expected set not error; got %v", err)
 	}
 
-	time.Sleep(5 * time.Millisecond)
+	time.Sleep(2 * time.Second)
 	b, err := s.Get("a")
 	if b != nil {
 		t.Fatalf("expected expired key return nil value; got %v", b)
@@ -23,8 +28,8 @@ func TestMemory(t *testing.T) {
 		t.Fatalf("expected expired key return error; got nil")
 	}
 
-	s.Set("a", []byte("test"), time.Millisecond)
-	time.Sleep(20 * time.Millisecond)
+	s.Set("a", []byte("test"), time.Second)
+	time.Sleep(2 * time.Second)
 	_, err = s.Get("a")
 	if err == nil {
 		t.Fatalf("expected expired key return error; got nil")
