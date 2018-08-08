74bc8a27c51fc94f0c6a9a04627255195a90f61e
acoshift <acoshift@gmail.com>
2018-08-08 14:12:07 +0700

    add test session get

--- session_internal_test.go
+++ session_internal_test.go
@@ -24,3 +24,23 @@ func TestSessionOperation(t *testing.T) {
 	assert.Equal(t, 1, s.Pop("b"))
 	assert.Nil(t, s.Get("b"))
 }
+
+func TestSessionGetWithTypes(t *testing.T) {
+	s := Session{}
+
+	s.Set("string", "text")
+	s.Set("int", 10)
+	s.Set("int64", int64(10))
+	s.Set("true", true)
+	s.Set("false", false)
+	s.Set("float32", float32(1.3))
+	s.Set("float64", float64(1.5))
+
+	assert.Equal(t, s.Get("string"), s.GetString("string"))
+	assert.Equal(t, s.Get("int"), s.GetInt("int"))
+	assert.Equal(t, s.Get("int64"), s.GetInt64("int64"))
+	assert.Equal(t, s.Get("true"), s.GetBool("true"))
+	assert.Equal(t, s.Get("false"), s.GetBool("false"))
+	assert.Equal(t, s.Get("float32"), s.GetFloat32("float32"))
+	assert.Equal(t, s.Get("float64"), s.GetFloat64("float64"))
+}
