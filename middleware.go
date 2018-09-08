b5fff22ae8c1dc5dbb0caa200d3cb1d435239b28
acoshift <acoshift@gmail.com>
2018-09-08 10:28:57 +0700

    refactor middleware

--- middleware.go
+++ middleware.go
@@ -11,11 +11,7 @@ var (
 	ErrNotPassMiddleware = errors.New("session: request not pass middleware")
 )
 
-type (
-	managerKey struct{}
-	requestKey struct{}
-	storageKey struct{}
-)
+type managerKey struct{}
 
 // Middleware is the Manager middleware wrapper
 //
@@ -30,23 +26,18 @@ func Middleware(config Config) func(http.Handler) http.Handler {
 func (m *Manager) Middleware() func(http.Handler) http.Handler {
 	return func(h http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			ctx := r.Context()
+			rm := requestedManager{
+				Manager: m,
+				r:       r,
+				storage: make(map[string]*Session),
+			}
 
-			// inject manager
-			ctx = context.WithValue(ctx, managerKey{}, m)
-
-			// inject request
-			ctx = context.WithValue(ctx, requestKey{}, r)
-
-			// inject session saver
-			storage := make(map[string]*Session)
-			ctx = context.WithValue(ctx, storageKey{}, storage)
-
+			ctx := context.WithValue(r.Context(), managerKey{}, &rm)
 			nr := r.WithContext(ctx)
 			nw := sessionWriter{
 				ResponseWriter: w,
 				beforeWriteHeader: func() {
-					for _, s := range storage {
+					for _, s := range rm.storage {
 						err := m.Save(w, s)
 						if err != nil {
 							panic("session: " + err.Error())
@@ -65,25 +56,34 @@ func (m *Manager) Middleware() func(http.Handler) http.Handler {
 
 // Get gets session from context
 func Get(ctx context.Context, name string) (*Session, error) {
-	m, _ := ctx.Value(managerKey{}).(*Manager)
+	m, _ := ctx.Value(managerKey{}).(*requestedManager)
 	if m == nil {
 		return nil, ErrNotPassMiddleware
 	}
 
 	// try get session from storage first
 	// to preserve session data from difference handler
-	storage := ctx.Value(storageKey{}).(map[string]*Session)
-	if s, ok := storage[name]; ok {
+	if s, ok := m.storage[name]; ok {
 		return s, nil
 	}
 
 	// get session from manager
-	s, err := m.Get(ctx.Value(requestKey{}).(*http.Request), name)
+	s, err := m.Get(name)
 	if err != nil {
 		return nil, err
 	}
 
 	// save session to storage for later get
-	storage[name] = s
+	m.storage[name] = s
 	return s, nil
+}
+
+type requestedManager struct {
+	*Manager
+	r       *http.Request
+	storage map[string]*Session
+}
+
+func (m *requestedManager) Get(name string) (*Session, error) {
+	return m.Manager.Get(m.r, name)
 }
