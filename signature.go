cc83b39ae6185af87eb032774d847dc5b4bf155d
acoshift <acoshift@gmail.com>
2018-10-29 12:00:36 +0700

    signature: use hmac.equal

--- signature.go
+++ signature.go
@@ -3,7 +3,6 @@ package session
 import (
 	"crypto/hmac"
 	"crypto/sha1"
-	"crypto/subtle"
 	"encoding/base64"
 )
 
@@ -16,7 +15,7 @@ func sign(value string, key []byte) string {
 
 func verify(value, digest string, keys [][]byte) bool {
 	for _, k := range keys {
-		if subtle.ConstantTimeCompare([]byte(digest), []byte(sign(value, k))) == 1 {
+		if hmac.Equal([]byte(digest), []byte(sign(value, k))) {
 			return true
 		}
 	}
